dbaf3968b73c325dad4e11f4c5b808d68e3eb727
Cem Alpturk <cem.alpturk@gmail.com>
2024-11-10 10:15:12 +0100

    makefile for dir generation

---
+++ the_kingdom_of_algorithmia/template.py
@@ -0,0 +1,23 @@
+def read_input(filename: str) -> list[str]:
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    return lines
+
+
+def part1() -> None:
+    pass
+
+
+def part2() -> None:
+    pass
+
+
+def part3() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
+    part3()
