8e7448b05fd4eec72c310eb444f30febea2ec221
Cem Alpturk <cem.alpturk@gmail.com>
2024-11-12 18:46:12 +0100

    update template

--- the_kingdom_of_algorithmia/template.py
+++ the_kingdom_of_algorithmia/template.py
@@ -1,20 +1,49 @@
+import os
+
+
 def read_input(filename: str) -> list[str]:
-    with open(filename, "r") as f:
+
+    filepath = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            filename,
+        )
+    )
+
+    with open(filepath, "r") as f:
         lines = f.readlines()
 
     return lines
 
 
 def part1() -> None:
-    pass
+    filename = "part1-test.txt"
+    # filename = "part1.txt"
+
+    lines = read_input(filename)
+
+    val = 0
+    print(f"Part1: {val}")
 
 
 def part2() -> None:
-    pass
+    filename = "part2-test.txt"
+    # filename = "part2.txt"
+
+    lines = read_input(filename)
+
+    val = 0
+    print(f"Part2: {val}")
 
 
 def part3() -> None:
-    pass
+    filename = "part3-test.txt"
+    # filename = "part3.txt"
+
+    lines = read_input(filename)
+
+    val = 0
+    print(f"Part3: {val}")
 
 
 if __name__ == "__main__":
