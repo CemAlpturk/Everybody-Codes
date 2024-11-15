e28f9b52ac873e251267c8a1332a2c07cb614a18
Cem Alpturk <cem.alpturk@gmail.com>
2024-11-15 17:08:46 +0100

    quest5 fix

--- the_kingdom_of_algorithmia/quest_5/quest_5.py
+++ the_kingdom_of_algorithmia/quest_5/quest_5.py
@@ -100,40 +100,50 @@ def part2() -> None:
     print(f"Part2: {res}")
 
 
+def dance_turn(round_nb: int, places: dict) -> None:
+    columns = len(places)
+    clap_col = round_nb % columns
+    clapper = places[clap_col].pop(0)
+    dancing_col = (clap_col + 1) % columns
+    side, idx = divmod(clapper, len(places[dancing_col]))
+    if (side % 2 and idx) or (not side % 2 and not idx):
+        if idx == 1:
+            places[dancing_col].append(clapper)
+        else:
+            places[dancing_col].insert(-idx + 1, clapper)
+    elif side % 2 or (not side % 2 and idx):
+        places[dancing_col].insert(idx - 1, clapper)
+
+
 def part3() -> None:
     filename = "part3.txt"
     lines = read_input(filename)
-    q1, q2, q3, q4 = [], [], [], []
 
+    places = {}
     for line in lines:
         line = line.strip("\n").split(" ")
-        q1.append(int(line[0]))
-        q2.append(int(line[1]))
-        q3.append(int(line[2]))
-        q4.append(int(line[3]))
+        for i, val in enumerate(line):
+            places[i] = places.get(i, [])
+            places[i].append(int(val.strip()))
 
-    qs = [q1, q2, q3, q4]
+    shouted = set()
+    config = {}
+    i = 0
+    columns = len(places)
+    while True:
+        conf = str(i % columns) + "-".join(
+            ["".join(str(x) for x in col) for col in places.values()]
+        )
+        if config.get(conf):
+            break
 
-    clapper_idx = 0
-    maxval = -1
-    for i in (pbar := tqdm(range(40000000))):
-        clapper = qs[clapper_idx].pop(0)
-        line_idx = (clapper_idx + 1) % 4
+        config[conf] = True
+        dance_turn(i, places)
+        number = "".join(str(col[0]) for col in places.values())
+        shouted.add(number)
+        i += 1
 
-        n = len(qs[line_idx])
-        c = clapper % (n * 2)
-        if c <= n:
-            qs[line_idx].insert(c - 1, clapper)
-        else:
-            if c == n + 1:
-                qs[line_idx].append(clapper)
-            else:
-                qs[line_idx].insert(1 - (c - n), clapper)
-        num = int("".join([str(q[0]) for q in qs]))
-        clapper_idx = (clapper_idx + 1) % 4
-        maxval = max(maxval, num)
-        if i % 10000:
-            pbar.set_description(str(maxval))
+    maxval = max(shouted)
 
     print(f"Part3: {maxval}")
 
